71556f5b7ac27d10fec1995e3d18d5c839c801f8
Ilona Student <None>
2019-04-12 10:12:59 -0700

    yeay it works but still have to change the IP address

--- legacy_router.py
+++ legacy_router.py
@@ -8,20 +8,20 @@ from mininet.log import setLogLevel, info
 
 def myNetwork():
 
-	net = Mininet( topo=None, build=False, ipBase='10.0.0.0/8')
+	net = Mininet( topo=None, build=False, ipBase='10.26.1.0/24')
 	info( '*** Adding controller\n' )
 	info( '*** Adding switches\n' )
-	r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+	r1 = net.addHost('r1', cls=Node, ip='10.26.1.1/24')
 	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 	info( '*** Add host\n')
-	h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-	h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+	h2 = net.addHost('h2', cls=Host, ip='10.26.1.2/24', defaultRoute='via 10.26.1.1')
+	h1 = net.addHost('h1', cls=Host, ip='10.26.2.2/24', defaultRoute='via 10.26.2.1')
 #they are on the same subnet
 
 	info( '***Add links\n')
-	net.addLink(h1, r1)
-	net.addLink(h2, r1)
+	net.addLink(h2, r1, intfName2='r1-eth1', params2={ 'ip' : '10.26.1.1/24'})
+	net.addLink(h1, r1, intfName2='r1-eth2', params2={ 'ip' : '10.26.2.1/24'})
 
 	info( '*** Starting network\n')
 	net.build()
